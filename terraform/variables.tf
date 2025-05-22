c17015748281adf9ef94885327ee1a8815c16d26
johalduran7 <johalduran@gmail.com>
2025-05-21 21:49:56 -0500

    updated (#1)
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    ---------
    
    Co-authored-by: John Duran <john.duran@nice.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,12 @@
 # In this file put the variables related to the deployment
-variable "variable_name" {
-    type = "type",
-    description = "Description"
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "env" {
+  default = "devel"
+}
+
+variable build_path {
+  default     = "/Users/johnduran/fsl_challenge/fsl-challenge-johnduran/app/build/"
 }
