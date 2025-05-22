8be5ea9761fb0a367aade4ab88d1cd5b2cdec7e1
John Duran <john.duran@nice.com>
2025-05-21 21:23:25 -0500

    updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 # In this file put the variables related to the deployment
 variable "aws_region" {
-    default= "us-east-1"
+  default = "us-east-1"
 }
 
 variable "env" {
-    default = "devel"
+  default = "devel"
 }
 
