124131098e44bee352b0a15d7135706c41516d27
John Duran <johalduran@gmail.com>
2025-05-21 21:23:25 -0500

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 
 
 resource "aws_s3_bucket" "frontend" {
-  bucket = "fsl-frontend-john-duran-1234-${var.env}"
-    force_destroy= true
+  bucket        = "fsl-frontend-john-duran-1234-${var.env}"
+  force_destroy = true
   tags = {
     Name = "fsl-frontend-john-duran-1234-${var.env}"
   }
@@ -27,23 +27,21 @@ locals {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
+    domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
 
   enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Some comment"
+  is_ipv6_enabled     = false
+  comment             = "CDN FSL ${var.env}"
   default_root_object = "index.html"
 
   logging_config {
     include_cookies = false
-    bucket          = "mylogs.s3.amazonaws.com"
-    prefix          = "myprefix"
+    bucket          = aws_s3_bucket.frontend_logs.bucket_regional_domain_name
   }
 
-  aliases = ["mysite.example.com", "yoursite.example.com"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
@@ -64,65 +62,88 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_200"
-
   restrictions {
     geo_restriction {
-      restriction_type = "whitelist"
-      locations        = ["US", "CA", "GB", "DE"]
+      restriction_type = "none"
+
     }
   }
 
   tags = {
-    Environment = "production"
+    Environment = var.env
   }
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.cloudfront_logs,
+    aws_s3_bucket_acl.cloudfront_logs_acl,
+    aws_s3_bucket_public_access_block.frontend_bucket
+  ]
 
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "FrontendOAC-${var.env}"
+  description                       = "FrontendOAC-${var.env}"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_s3_bucket" "frontend_logs" {
+  bucket        = "fsl-frontend-john-duran-1234-${var.env}-logs"
+  force_destroy = true
+  tags = {
+    Name = "fsl-frontend-john-duran-1234-${var.env}-logs"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_bucket_logs" {
+  bucket = aws_s3_bucket.frontend_logs.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "cloudfront_logs" {
+  bucket = aws_s3_bucket.frontend_logs.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+
+resource "aws_s3_bucket_acl" "cloudfront_logs_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.cloudfront_logs]
+
+  bucket = aws_s3_bucket.frontend_logs.id
+  acl    = "log-delivery-write"
+}
+
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = {
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = "s3:GetObject",
+        Resource = "${aws_s3_bucket.frontend.arn}/*",
+        Condition = {
+          StringEquals : {
+            "AWS:SourceArn" : aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    }
+  )
 }
